7f97e72b11667b3911ca729ed99d7b727e6cd191
Katelyn Herre <kherre@saintjoe.edu>
2015-02-13 12:23:30 -0600

    Wrote program to add total of 5 given numbers

--- stack.js
+++ stack.js
@@ -1,1 +1,16 @@
 //kherre practice-stack
+
+load('stack.js');
+var this = new Stack();
+this.push("2");
+this.push("4");
+this.push("6");
+this.push("8");
+this.push("10");
+var prev = this.length();
+
+
+for( var i = 0; i <prev; ++1)
+	runtotal += this.pop();
+prrint(runtotal);
+
